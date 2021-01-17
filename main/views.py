659aafd249ac0b84efcc2a9af3521937d9aa1302
Anselmo-2010 <you@example.com>
2021-01-17 14:43:36 +0600

    added todo  models and 3 pages html

--- main/views.py
+++ main/views.py
@@ -5,3 +5,12 @@ def homepage(request):
 
 def test(request):
     return render(request, "test.html")
+
+def page1(request):
+    return render(request, "page1.html")  
+
+def page2(request):
+    return render(request, "page2.html")  
+
+def page3(request):
+    return render(request, "page3.html") 
--- todo/urls.py
+++ todo/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import homepage, test
+from main.views import homepage, test, page1, page2, page3
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -24,5 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", homepage, name = "homepage"),
     path("test", test, name = "test"),
+    path("page1", page1, name = "page1"),
+    path("page2", page2, name = "page2"),
+    path("page3", page3, name = "page3")
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
